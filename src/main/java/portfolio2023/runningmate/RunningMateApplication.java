d28cb1c875517c645691b7e10c5bebf9ad1f53fb
noname_dev <noname_dev@naver.com>
2023-10-31 17:46:02 +0900

    러닝 메이트 프로젝트 생성

---
+++ src/main/java/portfolio2023/runningmate/RunningMateApplication.java
@@ -0,0 +1,13 @@
+package portfolio2023.runningmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/portfolio2023/runningmate/RunningMateApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolio2023.runningmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
