710b84ba7455782582fcf958952fc503f734a52d
noname_dev <noname_dev@naver.com>
2023-11-01 20:56:57 +0900

    회원가입 검증 추가

--- src/main/java/portfolio2023/runningmate/controller/AccountController.java
+++ src/main/java/portfolio2023/runningmate/controller/AccountController.java
@@ -1,18 +1,47 @@
 package portfolio2023.runningmate.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 import portfolio2023.runningmate.domain.dto.SignUpForm;
+import portfolio2023.runningmate.domain.validator.SignUpFormValidator;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/running-mate")
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(signUpFormValidator);
+    }
+
+    @GetMapping
+    public String runningMateHome(){
+        return "index";
+    }
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model){
         model.addAttribute("signUpForm", new SignUpForm());
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors){
+        if (errors.hasErrors()){
+            return "account/sign-up";
+        }
+
+
+        //TODO 회원 가입 처리
+        return "redirect:/running-mate";
     }
 }
---
+++ src/main/java/portfolio2023/runningmate/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package portfolio2023.runningmate.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import portfolio2023.runningmate.domain.Account;
+
+public interface AccountRepository extends JpaRepository<Account,Long> {
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+}
