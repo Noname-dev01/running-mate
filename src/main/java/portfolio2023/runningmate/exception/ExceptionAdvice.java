15739c3b8768886c9389f2a2421d7bc64f42d45b
noname_dev <noname_dev@naver.com>
2023-12-11 15:10:29 +0900

    에러 핸들러 및 뷰 추가

---
+++ src/main/java/portfolio2023/runningmate/exception/ExceptionAdvice.java
@@ -0,0 +1,25 @@
+package portfolio2023.runningmate.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import portfolio2023.runningmate.domain.Account;
+import portfolio2023.runningmate.security.CurrentAccount;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@ControllerAdvice
+public class ExceptionAdvice {
+
+    @ExceptionHandler
+    public String handleRuntimeException(@CurrentAccount Account account, HttpServletRequest req, RuntimeException e){
+        if (account != null){
+            log.info("'{}' requested '{}'", account.getNickname(), req.getRequestURI());
+        }else {
+            log.info("requested '{}'", req.getRequestURI());
+        }
+        log.error("bad request", e);
+        return "error";
+    }
+}
