bacda6807390952f24c8ca7b059ea32446e171aa
noname_dev <noname_dev@naver.com>
2023-12-15 22:52:30 +0900

    Jar 실행시 FileNotException 수정

--- src/main/java/portfolio2023/runningmate/service/ZoneService.java
+++ src/main/java/portfolio2023/runningmate/service/ZoneService.java
@@ -11,8 +11,10 @@ import portfolio2023.runningmate.domain.dto.ZoneForm;
 import portfolio2023.runningmate.repository.ZoneRepository;
 
 import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -30,11 +32,16 @@ public class ZoneService {
     public void initZoneData() throws IOException {
         if (zoneRepository.count() == 0){
             Resource resource = new ClassPathResource("zones_kr.csv");
-            List<Zone> zoneList = Files.readAllLines(resource.getFile().toPath(), StandardCharsets.UTF_8).stream()
-                    .map(line -> {
-                        String[] split = line.split(",");
-                        return Zone.builder().city(split[0]).localNameOfCity(split[1]).province(split[2]).build();
-                    }).collect(Collectors.toList());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+            List<Zone> zoneList = reader.lines().map(line -> {
+                String[] split = line.split(",");
+                return Zone.builder().city(split[0]).localNameOfCity(split[1]).province(split[2]).build();
+            }).collect(Collectors.toList());
+//            List<Zone> zoneList = Files.readAllLines(resource.getFile().toPath(), StandardCharsets.UTF_8).stream()
+//                    .map(line -> {
+//                        String[] split = line.split(",");
+//                        return Zone.builder().city(split[0]).localNameOfCity(split[1]).province(split[2]).build();
+//                    }).collect(Collectors.toList());
             zoneRepository.saveAll(zoneList);
         }
     }
